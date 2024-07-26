06d9c483de28636af35843313668d0a4d046e38d
Khushboo <khushbookarsh1112@gmail.com>
2024-07-26 14:03:59 +0530

    added some changes

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Icons from '../Icons/Icons'
 import "./Card.css"
 
-const Card = ({ player }) => {
+const Card = ({ player, onPlay, index, gameEnd }) => {
     let icons = <Icons/>
     if(player === 'X')
         icons = <Icons name="cross"/>
         else if(player === 'O')
             icons = <Icons name="circle"/>
   return (
-    <div className='card-wrapper'>
+    <div className='card-wrapper' onClick={() =>  !gameEnd && onPlay(index)}>
       {icons}
     </div>
   )
--- src/components/Grid/Grid.jsx
+++ src/components/Grid/Grid.jsx
@@ -1,17 +1,53 @@
 import { useState } from "react"
 import Card from "../Cards/Card";
 import "./Grid.css"
+import isWinner from "../isWinner/isWinner";
 
 
 function Grid({ numberofCards }) {
     let [ board, setBoard ] = useState(Array(numberofCards).fill(""));
+    let [ turn, setTurn] = useState(true);
+    let [ winner, setWinner] = useState(null);
+
+    function play(index) {
+      if(turn === true) {
+        board[index] = "X"
+      } else {
+        board[index] = "O"
+      }
+      let win = isWinner(board, turn ? "X" : "O")
+      if(win) {
+        setWinner(win)
+      }
+      setBoard([...board])
+      setTurn(!turn);
+    }
+
+    function reset() {
+      setBoard(Array(numberofCards).fill(""))
+      setTurn(true)
+      setWinner(null)
+    }
+
+
   return (
     <div className="grid-wrapper">
         {
-            board.map((item, index) => {
-                return <Card key ={index} player={item} />
-            })
+          winner && (
+            <>
+              <h1>Winner: {winner}</h1>
+              <button onClick={reset}>Reset game</button>
+            </>
+          )
         }
+        <h2>Current turn: {(turn) ? "X": "O"}</h2>
+        <div className="grid">
+          {
+            board.map((item, index) => {
+              return <Card gameEnd={(winner) ? true : false} key={index} player={item} onPlay={play} index={index} />
+          })
+          }
+        </div>
     </div>
   )
 }
